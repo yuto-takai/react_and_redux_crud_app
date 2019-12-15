727bfb63713101d18827f56831f65e64fb041492
YutoTakai <garakuta.guitar.ist@gmail.com>
2019-12-15 22:56:38 +0900

    reducerの実装

---
+++ src/reducers/count.js
@@ -0,0 +1,17 @@
+// Actionのタイプを取り込む
+import { INCREMENT, DECREMENT } from '../actions'
+
+// 初期状態のオブジェクト
+const initialState = { value: 0 }
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      // stateをインクリメントしたものをオブジェクトとしてかえす
+      return { value: state.value + 1 }
+    case DECREMENT:
+      return { value: state.value - 1 }
+    default:
+      return state
+  } 
+} 
