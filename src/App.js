f590960cb736d6e35a5fb7725a971fe8d509d33e
YutoTakai <garakuta.guitar.ist@gmail.com>
2019-12-15 20:08:07 +0900

    props tips

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <label htmlFor='bar'>bar</label>
-        <input type='text' onChange={() => {console.log('changed')}}></input>
-      </div>
-    )
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <label htmlFor='bar'>bar</label>
+//         <input type='text' onChange={() => {console.log('changed')}}></input>
+//       </div>
+//     )
+//   }
+// }
 
 // Class Component ↓
 
-// function App() {
-//   return (
-//     <div>
-//       <Cat/>
-//       <Cat/>
-//     </div>
-//   )
-// }
+function App() {
+  const profiles = [
+    { name: 'taro', age: 10 },
+    { name: 'john', age: 12 },
+    { name: 'NoName' }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
+  )
+}
 
-// const Cat = () => {
-//   return <div>Meow!!</div>
-// }
+const User = (props) => {
+  return <div>I am {props.name}, and {props.age} years old.</div>
+}
 
+User.defaultProps = {
+  age: 0
+}
  
 export default App;
 
