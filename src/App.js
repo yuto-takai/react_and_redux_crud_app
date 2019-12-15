fe08ae61459c975908bdd8b2a533ec0521dab51a
YutoTakai <garakuta.guitar.ist@gmail.com>
2019-12-15 19:05:13 +0900

    クラスコンポーネントと関数コンポーネントのちがい

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello. World</h1>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor='bar'>bar</label>
+        <input type='text' onChange={() => {console.log('changed')}}></input>
+      </div>
+    )
+  }
 }
+
+// Class Component ↓
+
+// function App() {
+//   return (
+//     <div>
+//       <Cat/>
+//       <Cat/>
+//     </div>
+//   )
+// }
+
+// const Cat = () => {
+//   return <div>Meow!!</div>
+// }
+
  
 export default App;
 
